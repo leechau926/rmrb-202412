639f04dfc557dd2de4e515478999bf4b117a46fc
Jack Stanley <2940675@163.com>
2024-12-10 14:44:37 +0800

    Update rmrbrmrb_date_v2412.sh

--- rmrbrmrb_date_v2412.sh
+++ rmrbrmrb_date_v2412.sh
@@ -16,15 +16,15 @@ echo ${day}
 echo "`date '+%Y-%m-%d %H:%M:%S'` process started."
 
 # download files
-# wget -i /home/leechau/down.txt -P /home/leechau/rmrb -a /home/leechau/rmrb/rmrb_down.log
+# wget -i /home/user/down.txt -P /home/user/rmrb -a /home/user/rmrb/rmrb_down.log
 # echo "`date '+%Y-%m-%d %H:%M:%S'` download files completed."
 
 # merge pdf files
-pdftk /home/leechau/rmrb/*.pdf cat output /home/leechau/rmrb${year}${month}${day}.pdf
+pdftk /home/user/rmrb/*.pdf cat output /home/user/rmrb${year}${month}${day}.pdf
 echo "`date '+%Y-%m-%d %H:%M:%S'` merge files completed."
 
 
 # rm files
-rm /home/leechau/rmrb/*.pdf
+rm /home/user/rmrb/*.pdf
 echo "`date '+%Y-%m-%d %H:%M:%S'` delete files completed."
 
